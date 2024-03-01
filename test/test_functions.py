96dc162b81b4f94c19c76fad68e65a789cd75504
Julia Gao <jgao3@caltech.edu>
2024-03-01 15:51:59 -0800

    added test cases mentioned in piazza

--- test/test_functions.py
+++ test/test_functions.py
@@ -88,6 +88,10 @@ class TestClass(unittest.TestCase):
         wb.set_cell_contents(n, "B1", "=INDIRECT(#CIRCREF!)")
         self.assertIsInstance(wb.get_cell_value(n, "B1"), sheets.CellError)
         self.assertEqual(wb.get_cell_value(n, "B1").get_type(), sheets.CellErrorType.BAD_REFERENCE)
+        
+        wb.set_cell_contents(n, "B2", "=INDIRECT(true)")
+        self.assertIsInstance(wb.get_cell_value(n, "B2"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(n, "B1").get_type(), sheets.CellErrorType.BAD_REFERENCE)
 
         
 
@@ -178,8 +182,26 @@ class TestClass(unittest.TestCase):
         wb.set_cell_contents(name, "D3", '=EXACT(#CIRCREF!, #ERROR!)')
         self.assertIsInstance(wb.get_cell_value(name, "D3"), sheets.CellError)
         self.assertEqual(wb.get_cell_value(name, "D3").get_type(), sheets.CellErrorType.PARSE_ERROR)
-
-
+        
+        wb.set_cell_contents(name, "F4", '=EXACT()')
+        self.assertIsInstance(wb.get_cell_value(name, "F4"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(name, "F4").get_type(), sheets.CellErrorType.TYPE_ERROR)
+        
+        wb.set_cell_contents(name, "F5", '=EXACT("ABC")')
+        self.assertIsInstance(wb.get_cell_value(name, "F5"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(name, "F5").get_type(), sheets.CellErrorType.TYPE_ERROR)
+        
+        wb.set_cell_contents(name, "F6", '=EXACT(#VALUE!, #VALUE!)')
+        self.assertIsInstance(wb.get_cell_value(name, "F6"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(name, "F6").get_type(), sheets.CellErrorType.TYPE_ERROR)
+        
+        wb.set_cell_contents(name, "F7", '=EXACT("a", "b", "c")')
+        self.assertIsInstance(wb.get_cell_value(name, "F6"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(name, "F6").get_type(), sheets.CellErrorType.TYPE_ERROR)
+        
+        wb.set_cell_contents(name, "F8", '=EXACT(123.000, "123")')
+        self.assertEqual(wb.get_cell_value(name, "F8"), True)
+        
     def test_if(self):
         wb = sheets.Workbook()
         _, name = wb.new_sheet()
@@ -205,6 +227,9 @@ class TestClass(unittest.TestCase):
         wb.set_cell_contents(name, "A7", '=IF(#CIRCREF!, 1, 0)')        
         self.assertIsInstance(wb.get_cell_value(name, "A7"), sheets.CellError)
         self.assertEqual(wb.get_cell_value(name, "A7").get_type(), sheets.CellErrorType.CIRCULAR_REFERENCE)
+        
+        wb.set_cell_contents(name, "A8", '=IF(TRUE, "")')
+        self.assertEqual(wb.get_cell_value(name, "A8"), "")
     
     def test_function_iferror(self):
         wb = sheets.Workbook()
@@ -265,6 +290,19 @@ class TestClass(unittest.TestCase):
 
         _, m = wb.new_sheet()
         self.assertEqual(wb.get_cell_value(n, "A4"), True)
+        
+        wb.set_cell_contents(n, "B1", '=ISBLANK(C1)')
+        wb.set_cell_contents(n, "C1", '#REF!')
+        self.assertEqual(wb.get_cell_value(n, "B1"), False)
+        self.assertIsInstance(wb.get_cell_value(n, "C1"), sheets.CellError)
+        self.assertEqual(wb.get_cell_value(n, "C1").get_type(), sheets.CellErrorType.BAD_REFERENCE)
+        
+        wb.set_cell_contents(n, "C2", '=ISBLANK(Nonexistent!A1)')
+        self.assertEqual(wb.get_cell_value(n, "C2"), False)
+        
+        wb.set_cell_contents(n, "C3", "'")
+        wb.set_cell_contents(n, "D4", '=ISBLANK(C3)')
+        self.assertEqual(wb.get_cell_value(n, "D4"), False)
         
     def test_choose(self):
         wb = sheets.Workbook()
@@ -450,7 +488,7 @@ class TestClass(unittest.TestCase):
     def test_indirect_bad_sheet(self):
         wb = sheets.Workbook()
         _, n = wb.new_sheet()
-        m = "test"
+        m = "TEST"
 
         wb.set_cell_contents(n, "A1", f'=INDIRECT("{m}!A1")')
 
