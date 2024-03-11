902537e97fac52b7a16c11eff518ccf227643337
Julia Gao <jgao3@caltech.edu>
2024-03-10 17:37:21 -0700

    feat: cell range class

---
+++ sheets/range.py
@@ -0,0 +1,20 @@
+from . import reference
+
+from .reference import Reference
+
+class CellRange:
+
+    def __init__(self, sheet_name: str, start_location: str, end_location: str):
+        self.sheet_name = sheet_name
+
+        start_location_initial = Reference.from_string(start_location)
+        end_location_initial = Reference.from_string(end_location)
+        
+        self.start_ref = Reference(min(start_location_initial.col, end_location_initial.col), min(start_location_initial.row, end_location_initial.row))
+        self.end_ref = Reference(max(start_location_initial.col, end_location_initial.col), max(start_location_initial.row, end_location_initial.row))
+
+    
+    def generate(self):
+        for row in range(self.start_ref.row, self.end_ref.row + 1):
+            for col in range(self.start_ref.col, self.end_ref.col + 1):
+                yield Reference(col, row)
